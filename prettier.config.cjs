79b4f9755d13d48586690cd433e99af2827db11b
kim-taewoo <acoustic0419@gmail.com>
2023-12-21 00:07:52 +0900

    DrunkEffect 가 적용된 프로젝트 완성단계

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { Canvas } from "@react-three/fiber";
-import "./App.css";
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { Home } from './components/Home'
 
 function App() {
   return (
     <>
-      <Canvas>
-        <mesh>
-          <torusKnotGeometry />
-          <meshNormalMaterial />
-        </mesh>
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [4, 2, 6],
+        }}
+      >
+        <Home />
       </Canvas>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
